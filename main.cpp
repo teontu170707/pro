33b67ac9e5a33c4549f97f8ad747424c911f4193
teontu170707 <120254897+teontu170707@users.noreply.github.com>
2022-12-17 15:46:34 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,105 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+//sir crescator 
+
+
+
+// {int a;
+// int cresc=1; 
+// int i=1;
+// int n;
+//  cout<<"n=";
+//  cin>>n;
+//  cout<<"a=";
+ 
+//  cin>>a;
+// int b;
+//  for (i=1; i<n; i++) 
+//   {
+//   b=a;
+//   cout<<"a=";
+//   cin>>a;
+   
+//   if (b>a)
+//     {
+//         cresc=0; 
+//     }
+   
+//   }
+  
+//  if (cresc==1)
+//   {
+//       cout<<"sirul este crescator";
+//   }
+//  else 
+//  {
+//      cout<<"neordonat";
+//  }
+//     return 0;
+// }
+
+
+// max dintr o secventa
+
+
+
+
+// {
+//     int n;
+//     int a;
+//     int i=1;
+//     int max;
+//     cout<<"n=";
+//     cin>>n;
+//     cout<<"a"<<i<<"=";
+//     cin>>a;
+//     max=a;
+//     i++;
+//     do
+//     {max=a;
+//      cout<<"a"<<i<<"=";
+//      cin>>a;
+//      i++;
+//      if(a>=max)
+//       { max=a;
+//       }
+//      
+//     }while( i<n); 
+   
+//     cout<<"maximul este"<<" "<<max;
+
+//numere pare
+
+
+//{int n;
+ int a;
+ int i=1;
+ int p=0;
+ cout<<"n=";
+ cin>>n;
+
+ for(i=1; i<=n; i++)
+ {  cout<<"a"<<i<<"=";
+     cin>>a;
+     if(a%2==0) 
+     {
+         p=p+1;
+     }
+    
+ }
+ cout<<"numarul de numere pare este"<<" "<<p;
+    return 0;
+//}
+

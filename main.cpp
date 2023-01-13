1a5a722ba0e53066351cfe3a93842d600f15a401
teontu170707 <120254897+teontu170707@users.noreply.github.com>
2023-01-13 23:18:00 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,91 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+ //1.sir crescator cu while
+ 
+  { 
+//      int a;
+//      int b;
+//     int i=1;
+//     int cresc=1;
+//     int n;
+//     cout<<"n=";
+//     cin>>n;
+//     cout<< "a=";
+//     cin>>a;
+    
+//     while(i<n)
+//      {i++;
+//      b=a;
+//      cout<<"a=";
+//      cin>>a;
+          
+//       }
+//     if(b<a)
+//     {
+//      cresc=0; 
+//      cout<<"sirul este crescator";
+//     }
+//       else cout<<"sirul nu este crescator";
+    
+    //SIR CRESCATOR CU FOR.
+    
+    //  int a;
+    //  int b;
+    //  int i=1;
+    //  int n;
+    //  int cresc=1;
+    //  cout<<"n=";
+    //  cin>>n;
+    //  cout<<"a=";
+    //  cin>>a;
+    //  for(int i=1; i<n; i++)
+    //   {
+    //       b=a;
+    //       cout<<"a=";
+    //       cin>>a;
+          
+    //   }
+    //  if(b<a)
+    //   {
+    //       cresc=1;
+    //       cout<<"sir cresc";
+    //   }
+    //  else cout<<"sir necresc";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      return 0;
+         
+      }
+    
+   
+
+

8a40c13c7c07a91f9d37e88c2d6dcadd51620c6b
teontu170707 <120254897+teontu170707@users.noreply.github.com>
2023-01-28 15:51:47 +0200

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,69 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{//eliminarea unui element din vector
+//     int n;
+//     int a[1000];
+//     int p;
+//     cout<<"n=";
+//     cin>>n;
+//     for(int i=1; i<=n; i++)
+//     {cout<<"a["<<i<<"]=";
+//      cin>>a[i];
+//     }
+//   cout<<"dati pozitia care doriti sa fie eliminata"<<" "<<"p=";
+//   cin>>p;
+//   n--;
+//   
+//   for(int i=1; i<=n; i++)
+//     {cout<<"a["<<i<<"]="<<a[i]<<endl;}
+
+  //eliminarea unei valori anume dintr un vector
+    //   int n;
+    //   int a[1000];
+    //   int e;
+    //   cout<<"n=";
+    //   cin>>n;
+    //   for(int i=1; i<=n; i++)
+    //   {cout<<"a["<<i<<"]=";
+    //     cin>>a[i];
+    //   }
+    //   cout<<"dati elementul care doriti sa fie eliminat"<<" "<<"e="; 
+    //   cin>>e;
+    //   for(int i=1; i<=n; i++)
+    //   {while (a[i]==e)
+    //     {
+    //     for(int j=i; j<n; j++)
+    //       {a[j]=a[j+1];}
+    //      n--;
+        
+    //     }
+        
+        
+    //   }
+    //   for(int i=1; i<=n; i++)
+    //   {cout<<"a["<<i<<"]="<<a[i]<<endl;
+          
+    //   }
+   //algoritm care sa gaseasca al doilea cel mai mare element din vector  PT ACASA
+    // int n;
+    // int a[100];
+    // int m;
+    // cout<<"n=";
+    // cin>>n;
+    // for(int i=1; i<=n;i++)
+    // {cout<<"a["<<i<<"]="
+        
+    // }
+    return 0;
+}
